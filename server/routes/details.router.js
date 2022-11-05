e342a71a02874b8597e25aee70f5c79d1fee7cab
Daniel Legan <legan.daniel@gmail.com>
2022-11-05 10:56:45 -0500

    collected required info from database per movie id

--- server/routes/details.router.js
+++ server/routes/details.router.js
@@ -5,7 +5,24 @@ const pool = require('../modules/pool')
 router.get('/', (req, res) => {
     const movieId = req.query.query;
     console.log('movieID', movieId);
-    res.sendStatus(500)
+    // want to query for title, description, poster, and all genres for the movie
+    const sqlText = `
+                    SELECT 
+                        movies.*, 
+                        json_agg(genres) AS genres
+                    FROM movies
+                    LEFT JOIN movies_genres
+                        ON movies_genres.movie_id = movies.id
+                    LEFT JOIN genres
+                        ON movies_genres.genre_id = genres.id
+                    WHERE movies.id = $1
+                    GROUP BY movies.id;
+                    `;
+    pool.query(sqlText, [movieId])
+        .then(dbRes => {
+            // send back the first item in the array
+            res.send(dbRes.rows[0]);
+        })
 });
 
 module.exports = router;
