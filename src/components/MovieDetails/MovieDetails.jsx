0facefd3e748cfaee1de03b82230abe275da0ba8
Daniel Legan <legan.daniel@gmail.com>
2022-11-05 11:42:09 -0500

    able to display movie properties

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -4,7 +4,10 @@ import { useDispatch, useSelector } from 'react-redux';
 
 function MovieDetails() {
     const params = useParams();
+    const movieDetails = useSelector(store => store.movieDetails);
     const dispatch = useDispatch();
+
+
     console.log('params', params); // { id: '17' } as an example
 
     useEffect(() => {
@@ -14,8 +17,18 @@ function MovieDetails() {
         })
     }, [params.id]);
 
+    console.log('movieDetails', movieDetails);
+
     return (
-        <div>MovieDetails with movie id: {params.id}</div>
+        <>
+            {/* <div>MovieDetails with movie id: {params.id}</div> */}
+            <p><b>Movie Title: </b>{movieDetails.title}</p>
+            <img src={movieDetails.poster} />
+            <p><b>Movie Description: </b>{movieDetails.description}</p>
+            <p><b>Movie Genres: </b></p>
+            {/* IMPORTANT! without conditional, .map() has undefined initially, takes a bit to retrieve array with useSelector */}
+            {movieDetails.genres && movieDetails.genres.map(item => (<p key={item.id}>{item.name}</p>))}
+        </>
     );
 }
 
